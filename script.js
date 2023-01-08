e2eaacc344c26570bb1d4e9311af92cd603b5522
Makktu <johnmcna@gmail.com>
2023-01-08 20:07:59 +0000

    added placeholder msg and default cursor pos

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 const menuBar = document.querySelector(".menu-items");
 const textEntryArea = document.querySelector(".text-entry-area");
 const lightDarkModeBtn = document.getElementById("light-dark-btn");
+const fileBtn = document.getElementById("file-btn");
+textEntryArea.focus(); //set the focus - cursor at end
+
 let darkModeOn = true;
 
 lightDarkModeBtn.addEventListener("click", () => {
@@ -17,4 +20,8 @@ lightDarkModeBtn.addEventListener("click", () => {
         darkModeOn = true;
         lightDarkModeBtn.textContent = "Light Mode";
     }
+});
+
+fileBtn.addEventListener("click", () => {
+    console.log("File clicked");
 });
